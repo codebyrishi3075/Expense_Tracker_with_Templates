35fc4c1b5ed31ab827303875c4f5bc78b0127474
Rishikesh <codebyrishi3075@gmail.com>
2026-02-23 17:14:11 +0530

    Second commit

--- proj_expense_tracker/urls.py
+++ proj_expense_tracker/urls.py
@@ -5,4 +5,15 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('expenses/', include('expenses.urls')),
+    path('budget/', include('budget.urls')),
+    path('dashboard/', include('dashboard.urls')),
+    path('contact_app/', include('contact_app.urls')),
+    path('userSettings/', include('userSettings.urls')),
 ]
+
+
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
